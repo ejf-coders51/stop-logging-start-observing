b66359f76560c6b68c6e2b361204ac57e4327593
ejf-coders51 <ejfrancischetti@coders51.com>
2023-01-16 14:00:06 +0100

    v1
    draft
    
    clean
    
    Readme
    
    Fix Readme
    
    Readme
    
    Slides
    
    final version

--- init.sh
+++ init.sh
@@ -2,16 +2,21 @@
 
 set -e
 
-echo "########################################"
-echo "CREATE CLUSTER"
-echo "########################################"
-kind create cluster --config ./kind/kind-cluster.yaml
-sleep 5
+helm repo add grafana https://grafana.github.io/helm-charts 
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add cert-manager https://charts.jetstack.io
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+helm repo add traefik https://traefik.github.io/charts
+helm repo add bitnami https://charts.bitnami.com/bitnami 
+helm repo update
 
 echo "########################################"
 echo "GRAFANA"
 echo "########################################"
-kubectl apply -f ./grafana/secret.yaml
+kubectl apply -f grafana/secret.yaml
+sleep 5
+kubectl apply -f grafana/pizzify-dashboard.yaml
 sleep 5
 helm upgrade --install grafana grafana/grafana -f grafana/values.yaml
 sleep 2
@@ -19,15 +24,16 @@ sleep 2
 echo "########################################"
 echo "PROMETHEUS"
 echo "########################################"
-helm upgrade --install prometheus-stack prometheus-community/kube-prometheus-stack --set grafana.enabled=false,coreDns.enabled=false,kubeControllerManager.enabled=false,kubeDns.enabled=false,kubeEtcd.enabled=false,kubeProxy.enabled=false,kubeScheduler.enabled=false,nodeExporter.enabled=false,alertmanager.enabled=false
+helm upgrade --install prometheus-stack prometheus-community/kube-prometheus-stack -f prometheus/values.yaml
 sleep 10
-kubectl apply -f ./prometheus
+kubectl apply -f prometheus/prometheus-auto-discovery.yaml
+kubectl apply -f prometheus/prometheus-stack-roles.yaml
 sleep 2
 
 echo "########################################"
 echo "LOKI"
 echo "########################################"
-helm upgrade --install loki-stack grafana/loki-stack
+helm upgrade --install loki-stack grafana/loki-stack -f loki/values.yaml
 sleep 2
 
 echo "########################################"
@@ -41,20 +47,20 @@ echo "########################################"
 echo "JAEGER"
 echo "########################################"
 helm upgrade --install jaeger jaegertracing/jaeger-operator --set jaeger.create=true,rbac.clusterRole=true
-sleep 2
+sleep 5
 
 echo "########################################"
 echo "OPENTELEMETRY"
 echo "########################################"
-helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator
-sleep 20
+helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator 
+sleep 90
 kubectl apply -f opentelemetry/otel-collector.yaml
-sleep 2
+sleep 5
 
 echo "########################################"
 echo "TRAEFIK"
 echo "########################################"
-helm upgrade --install traefik traefik/traefik -f ./traefik/values.yaml
+helm upgrade --install traefik traefik/traefik -f traefik/values.yaml
 sleep 2
 
 kubectl apply -f traefik/traefik-routing.yaml
@@ -65,7 +71,20 @@ echo "########################################"
 kubectl rabbitmq install-cluster-operator
 sleep 10
 kubectl rabbitmq create rabbitmq --replicas 1
-sleep 90
+sleep 120
 kubectl exec -it svc/rabbitmq -- rabbitmqctl add_user 'app' 'password'
 kubectl exec -it svc/rabbitmq -- rabbitmqctl set_permissions -p "/" app ".*" ".*" ".*"
 kubectl exec -it svc/rabbitmq -- rabbitmqctl set_user_tags app  monitoring
+
+echo "########################################"
+echo "POSTGRES"
+echo "########################################"
+helm upgrade --install db-order bitnami/postgresql --set global.postgresql.auth.postgresPassword=postgres --set global.postgresql.auth.database=pizzify_order_dev
+helm upgrade --install db-job bitnami/postgresql --set global.postgresql.auth.postgresPassword=postgres --set global.postgresql.auth.database=pizzify_job_dev
+sleep 10
+
+echo "########################################"
+echo "PIZZIFY"
+echo "########################################"
+kubectl apply -f pizzify/
+sleep 30
